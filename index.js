035d36b1d2aeae6e605ace8c483f7d9216fa69b8
Richie Bendall <richiebendall@gmail.com>
2025-09-07 08:34:30 +1200

    Nest all under `[Symbol.asyncIterator]()`

--- index.js
+++ index.js
@@ -9,58 +9,57 @@ export default function domMutations(target, options = {}) {
 }
 
 export function batchedDomMutations(target, {signal, ...options} = {}) {
-	signal?.throwIfAborted();
-
-	const resolvers = [];
-
-	let isDone = false;
-
-	const observer = new globalThis.MutationObserver(mutations => {
-		const next = resolvers.shift();
-
-		if (next) {
-			next.resolve({value: mutations, done: false});
-		}
-	});
-
-	observer.observe(target, options);
-
-	signal?.addEventListener('abort', () => {
-		isDone = true;
-		while (resolvers.length > 0) {
-			const next = resolvers.shift();
-			next.reject(signal.reason);
-		}
-
-		observer.disconnect();
-	}, {once: true});
-
 	return {
-		async next() {
-			if (isDone) {
-				return {value: undefined, done: true};
-			}
-
+		[Symbol.asyncIterator]() {
 			signal?.throwIfAborted();
 
-			return new Promise((resolve, reject) => {
-				resolvers.push({resolve, reject});
+			const resolvers = [];
+			let isDone = false;
+
+			const observer = new globalThis.MutationObserver(mutations => {
+				const next = resolvers.shift();
+				if (next) {
+					next.resolve({value: mutations, done: false});
+				}
 			});
-		},
-		async return(value) {
-			isDone = true;
 
-			observer.disconnect();
+			observer.observe(target, options);
 
-			return {value, done: true};
-		},
-		async throw(error) {
-			await this.return();
+			signal?.addEventListener('abort', () => {
+				isDone = true;
+				while (resolvers.length > 0) {
+					const next = resolvers.shift();
+					next.reject(signal.reason);
+				}
 
-			throw error;
-		},
-		[Symbol.asyncIterator]() {
-			return this;
+				observer.disconnect();
+			}, {once: true});
+
+			const iterator = {
+				async next() {
+					if (isDone) {
+						return {value: undefined, done: true};
+					}
+
+					signal?.throwIfAborted();
+					return new Promise((resolve, reject) => {
+						resolvers.push({resolve, reject});
+					});
+				},
+				async return(value) {
+					isDone = true;
+					observer.disconnect();
+					return {value, done: true};
+				},
+				async throw(error) {
+					await this.return();
+					throw error;
+				},
+				[Symbol.asyncIterator]() {
+					return this;
+				},
+			};
+			return iterator;
 		},
 	};
 }
